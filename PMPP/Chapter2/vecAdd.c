d03f5ee43fa98a6170c4d738a50e49337af97df0
etmwb <1026014376@qq.com>
2020-03-23 22:37:36 +0800

    Update vecAdd.c

--- PMPP/Chapter2/vecAdd.c
+++ PMPP/Chapter2/vecAdd.c
@@ -1,4 +1,24 @@
 #include <cuda.h> 
 
-void vecAdd()
-{}
+void vecAdd(float* A, float* B, float* C, int n)
+{
+    int size = n * sizeof(float); 
+    float *d_A, *d_B, d_C;
+    cudaMalloc((void **) &d_A, size);
+    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice); 
+    cudaMalloc((void **) &d_B, size);
+    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice); 
+    
+    cudaMalloc((void **) &d_C, size);
+    vecAddKernel<<<ceil(n/256.0), 256>>>(d_A, d_B, d_C, n);
+    
+    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost); 
+    cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
+}
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, int n)
+{
+    int i = blockDim.x*blockIdx.x + threadIdx.x; 
+    if(i < n) C[i] = A[i] + B[i];
+}
