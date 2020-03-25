4d88635b0e3b58077869cb147cb1da78acea1a70
etmwb <1026014376@qq.com>
2020-03-25 18:11:28 +0800

    Create colorToGrey.c

---
+++ PMPP/Chanpter3/colorToGrey.c
@@ -0,0 +1,16 @@
+__global__ 
+void colorToGreyscaleConversion(unsigned char* Pout, unsigned char* Pin, 
+                                int width, int height)
+{
+    int Col = threadIdx.x + blockIdx.x * blockDim.x; 
+    int Row = threadIdx.y + blockIdx.y * blockDim.y; 
+    if (Col < width && Row < height)
+    {
+        int greyOffset = Row * width + Col;
+        int rgbOffset = greyOffset * CHANNELS;
+        unsigned char r = Pin[rgbOffset    ]; 
+        unsigned char g = Pin[rgbOffset + 1]; 
+        unsigned char b = Pin[rgbOffset + 2];
+        Pout[greyOffset] = 0.21f*r + 0.71f*g + 0.07f*b;
+    }
+}
